f057e84a20cfdb75f5a908ea2b56163c18136311
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2019-12-30 21:15:52 -0300

    Content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './content/sidebar/sidebar.component';
+import { ResumeComponent } from './content/resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
